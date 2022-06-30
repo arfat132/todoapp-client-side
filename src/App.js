5726f3f27188935b83f8989687be79d58a74dc93
arfat132 <arfatbegum90@gmail.com>
2022-07-01 00:07:20 +0300

    update todo implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import ToDo from './components/ToDo';
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Calender from './components/Calender';
+
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
-      <Route path="/" element={<ToDo />}>
-        
-      </Route>
-    </Routes>
+        <Route path="/" element={<ToDo />} />
+        <Route path="/calender" element={<Calender />} />
+      </Routes>
     </div>
   );
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,16 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function ToDo() {
     const [itemText, setItemText] = useState('');
+    const [newDate, setNewDate] = useState('');
     const [listItems, setListItems] = useState([]);
     const [isUpdating, setIsUpdating] = useState('');
+    const [updateItemText, setUpdateItemText] = useState('');
 
-    
+
     const addItem = async (e) => {
         e.preventDefault();
+        const todo = {
+            item: itemText,
+            date: newDate,
+
+        }
+        console.log(todo)
         try {
-            const res = await axios.post('http://localhost:5000/', { item: itemText })
+            const res = await axios.post('http://localhost:5000/',
+            todo)
             setListItems(prev => [...prev, res.data]);
             setItemText('');
         } catch (err) {
@@ -30,6 +39,7 @@ function ToDo() {
         getItemsList()
     }, []);
 
+
     const deleteItem = async (id) => {
         try {
             const res = await axios.delete(`http://localhost:5000/${id}`)
@@ -40,15 +50,36 @@ function ToDo() {
         }
     }
 
+    const updateItem = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await axios.put(`http://localhost:5000/${isUpdating}`, { item: updateItemText })
+            console.log(res.data)
+            const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
+            const updatedItem = listItems[updatedItemIndex].item = updateItemText;
+            setUpdateItemText('');
+            setIsUpdating('');
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
-  
-   
+    const updateTodohandle = () => (
+        <form className="p-4" onSubmit={(e) => { updateItem(e) }} >
+            <label>
+                <input type="checkbox" class="checkbox" />
+            </label>
+            <input className="input input-bordered w-full max-w-xs ml-4" type="text" placeholder="New Item" onChange={e => { setUpdateItemText(e.target.value) }} value={updateItemText} />
+            <button className="btn ml-4" type="submit">Update</button>
+        </form>
+    )
 
     return (
         <div className="App">
             <h1 className='text-xl font-bold capitalize mt-12 mb-6'>Todo List</h1>
             <form className="form" onSubmit={e => addItem(e)}>
                 <input type="text" className='input input-bordered w-full max-w-xs' placeholder='Add Todo Item' onChange={e => { setItemText(e.target.value) }} value={itemText} />
+                <input type="date" className='input input-bordered w-full max-w-xs'  onChange={e => { setNewDate(e.target.value) }} value={newDate} />
                 <button className='px-6 py-2.5 ml-2 rounded-lg bg-green-500 text-white font-bold' type="submit">Add</button>
             </form>
             <div className="overflow-x-auto mx-auto mt-12 w-[400px] lg:w-[1000px] lg:px-24">
@@ -57,27 +88,28 @@ function ToDo() {
                         <th className='grid grid-cols-3 gap-12'>
                             <span className='ml-12 col-span-2'>Name</span>
                             <div>
-                            <span className='ml-8'>Update</span>
-                            <span className='ml-8'>Delete</span>
+                                <span className='ml-8'>Update</span>
+                                <span className='ml-8'>Delete</span>
                             </div>
                         </th>
-
-
                     </thead>
                     <tbody>
-
                         {
                             listItems.map(item => (
                                 <div className="todo-item">
-                                    { <tr className='grid grid-cols-3 gap-6'>
+                                    {
+                                        isUpdating === item._id
+                                            ? updateTodohandle()
+                                            : <tr className='grid grid-cols-3 gap-6'>
                                                 <td className='flex col-span-2'>
                                                     <label>
                                                         <input type="checkbox" class="checkbox" />
                                                     </label>
                                                     <p className="item-content ml-4">{item.item}</p>
+                                                    <p>{item.date}</p>
                                                 </td>
                                                 <td className='ml-8'><button className="update-item" onClick={() => { setIsUpdating(item._id) }}>Update</button>
-                                                <button className="ml-4" onClick={() => { deleteItem(item._id) }}>Delete</button></td>
+                                                    <button className="ml-4" onClick={() => { deleteItem(item._id) }}>Delete</button></td>
                                             </tr>
                                     }
                                 </div>
